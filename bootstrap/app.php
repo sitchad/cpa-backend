ae9b2cc4dc7ce09870c1be22f9e6887d3e6f2145
TonPseudoGitHub <ton-email@example.com>
2026-06-12 20:35:38 +0100

    fix: register anti.fraud and role middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,3 @@
-<?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'anti.fraud' => \App\Http\Middleware\AntiFraud::class,
+            'role'       => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
